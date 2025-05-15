f5eac09c133f5b3dff926018e3e5c1ee94c48629
Alexander Kepekci <alexkepekci@hotmail.com>
2025-05-14 23:47:22 -0400

    moar eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -32,7 +32,9 @@ export default defineConfig([
       "css-modules/no-unused-class": "warn",
       "css-modules/no-undef-class": "warn",
       "react/jsx-no-comment-textnodes": "warn",
+      "react/no-unescaped-entities": "off",
       "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-unused-vars": "warn",
     },
   },
 ]);
